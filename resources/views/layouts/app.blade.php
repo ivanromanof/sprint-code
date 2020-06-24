077984307613843c4adfad42b5244b490473db85
ivanromanof <ivanromanov123@yandex.ru>
2020-06-24 22:09:12 +0300

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,11 +6,12 @@
     <title>Code Challenge - @yield('title')</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="csrf-param" content="_token" />
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;600&display=swap" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="{{ asset('js/app.js') }}"></script>
   </head>
   <body>
-    <div class="container mt-4">
+    <div>
       <h1>@yield('header')</h1>
       <div>
         @yield('content')
